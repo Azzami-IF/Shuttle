af7feafdd31c646891c9bbb9af4b80becc2f64b5
Azzami-IF <habiburrazami@gmail.com>
2026-06-13 19:33:41 +0700

    chore(shared): use BottomNavComponent as standalone and import it from SharedModule

--- IONIC/src/app/components/shared/bottom-nav/bottom-nav.component.ts
+++ IONIC/src/app/components/shared/bottom-nav/bottom-nav.component.ts
@@ -9,8 +9,8 @@ import { AuthService } from '../../../services/auth.service';
   selector: 'app-bottom-nav',
   templateUrl: './bottom-nav.component.html',
   styleUrls: ['./bottom-nav.component.scss'],
-  // converted to a declared component (not standalone) so it can be
-  // included in SharedModule declarations
+  standalone: true,
+  imports: [CommonModule, RouterModule]
 })
 export class BottomNavComponent implements OnInit {
   private nav = inject(NavService);
